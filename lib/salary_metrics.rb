26c74bac63da942e0c15062a821cc865b6a22523
P-one-1 <bpawan96@gmail.com>
2026-03-25 19:32:01 +0000

    refactor: clean up metrics query logic

--- lib/salary_metrics.rb
+++ lib/salary_metrics.rb
@@ -2,30 +2,32 @@ require_relative 'database'
 
 class SalaryMetrics
   def self.by_country(country)
-    rows = DB[:employees].where(country: country).select(:salary).all
-
-    return nil if rows.empty?
-
-    salaries = rows.map { |r| r[:salary].to_f }
+    salaries = fetch_salaries(DB[:employees].where(country: country))
+    return nil if salaries.empty?
 
     {
       country: country,
       minimum: salaries.min.round(2),
       maximum: salaries.max.round(2),
-      average: (salaries.sum / salaries.size).round(2)
+      average: average(salaries)
     }
   end
 
-	def self.by_job_title(job_title)
-    rows = DB[:employees].where(job_title: job_title).select(:salary).all
-
-    return nil if rows.empty?
-
-    salaries = rows.map { |r| r[:salary].to_f }
+  def self.by_job_title(job_title)
+    salaries = fetch_salaries(DB[:employees].where(job_title: job_title))
+    return nil if salaries.empty?
 
     {
       job_title: job_title,
-      average:   (salaries.sum / salaries.size).round(2)
+      average:   average(salaries)
     }
+  end
+
+  private_class_method def self.fetch_salaries(dataset)
+    dataset.select(:salary).all.map { |r| r[:salary].to_f }
+  end
+
+  private_class_method def self.average(salaries)
+    (salaries.sum / salaries.size).round(2)
   end
 end
