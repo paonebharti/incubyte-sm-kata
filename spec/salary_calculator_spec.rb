dac0f94d9bab81a15587a088ffca6f024bb88e84
P-one-1 <bpawan96@gmail.com>
2026-03-25 19:01:22 +0000

    test: salary calculation - no deduction for other countries

--- spec/salary_calculator_spec.rb
+++ spec/salary_calculator_spec.rb
@@ -13,6 +13,12 @@ RSpec.describe SalaryCalculator do
     result[:employee]
   end
 
+  let(:other_employee) do
+    result = Employee.create(full_name: 'Maria Garcia', job_title: 'Designer',
+                             country: 'Spain', salary: 100_000)
+    result[:employee]
+  end
+
   describe 'India deduction' do
     it 'applies 10% TDS for India' do
       result = SalaryCalculator.calculate(india_employee)
@@ -39,6 +45,18 @@ RSpec.describe SalaryCalculator do
     it 'returns correct net salary for United States' do
       result = SalaryCalculator.calculate(us_employee)
       expect(result[:net_salary]).to eq(88_000.0)
+    end
+  end
+
+  describe 'other countries' do
+    it 'applies no deduction for countries not in the rules' do
+      result = SalaryCalculator.calculate(other_employee)
+      expect(result[:tds]).to eq(0.0)
+    end
+
+    it 'returns net salary equal to gross for other countries' do
+      result = SalaryCalculator.calculate(other_employee)
+      expect(result[:net_salary]).to eq(100_000.0)
     end
   end
 end
